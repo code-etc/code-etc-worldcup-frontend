da5bd730f4ac7e9df10dce360e467d4f486b5315
daehwan2 <dhdh0482@naver.com>
2022-02-07 16:39:28 +0900

    Feat: mock api로 더미데이터 받아오기

--- app/src/routes/Home.jsx
+++ app/src/routes/Home.jsx
@@ -1,117 +1,67 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Slider from "../components/Slider";
+
 const Home = () => {
-  const datas = [
-    {
-      id: 1,
-      title: "라면 월드컵",
-      image: "",
-      write: "",
-      date: "",
-    },
-    {
-      id: 2,
-      title: "냉장고 월드컵",
-      image: "",
-      write: "",
-      date: "",
-    },
-    {
-      id: 3,
-      title: "햇반 월드컵",
-      image: "",
-      write: "",
-      date: "",
-    },
-    {
-      id: 4,
-      title: "김치 월드컵",
-      image: "",
-      write: "",
-      date: "",
-    },
-    {
-      id: 5,
-      title: "소주 월드컵",
-      image: "",
-      write: "",
-      date: "",
-    },
-    {
-      id: 6,
-      title: "막걸리 월드컵",
-      image: "",
-      write: "",
-      date: "",
-    },
-    {
-      id: 7,
-      title: "만화 월드컵",
-      image: "",
-      write: "",
-      date: "",
-    },
-    {
-      id: 8,
-      title: "영화 월드컵",
-      image: "",
-      write: "",
-      date: "",
-    },
-    {
-      id: 9,
-      title: "재미 월드컵",
-      image: "",
-      write: "",
-      date: "",
-    },
-    {
-      id: 10,
-      title: "농구 월드컵",
-      image: "",
-      write: "",
-      date: "",
-    },
-    {
-      id: 11,
-      title: "2020 월드컵",
-      image: "",
-      write: "",
-      date: "",
-    },
-    {
-      id: 12,
-      title: "안주 월드컵",
-      image: "",
-      write: "",
-      date: "",
-    },
-    {
-      id: 13,
-      title: "TV 월드컵",
-      image: "",
-      write: "",
-      date: "",
-    },
-    {
-      id: 14,
-      title: "2024 월드컵",
-      image: "",
-      write: "",
-      date: "",
-    },
-    {
-      id: 15,
-      title: "아이스크림 월드컵",
-      image: "",
-      write: "",
-      date: "",
-    },
-  ];
+  const [worldcupLists, setWorldcupLists] = useState([]);
+  const [selectLists, setSelectLists] = useState([]);
+  const divisionWorldcupList = (arr) => {
+    const maxLength = 15;
+    for (let i = 0; i < parseInt(arr.length / maxLength) + 1; i++) {
+      let list = [];
+      if (i === parseInt(arr.length / maxLength)) {
+        for (let j = 0; j < arr.length % maxLength; j++) {
+          list[j] = arr[j + i * maxLength];
+        }
+      } else {
+        for (let j = 0; j < maxLength; j++) {
+          list[j] = arr[j + i * maxLength];
+        }
+      }
+      setWorldcupLists((prev) => [...prev, list]);
+      console.log(list);
+    }
+  };
+  const divisionSelectList = (arr) => {
+    const maxLength = 15;
+    for (let i = 0; i < parseInt(arr.length / maxLength) + 1; i++) {
+      let list = [];
+      if (i === parseInt(arr.length / maxLength)) {
+        for (let j = 0; j < arr.length % maxLength; j++) {
+          list[j] = arr[j + i * maxLength];
+        }
+      } else {
+        for (let j = 0; j < maxLength; j++) {
+          list[j] = arr[j + i * maxLength];
+        }
+      }
+      setSelectLists((prev) => [...prev, list]);
+      console.log(list);
+    }
+  };
+  const getWorldcupList = async () => {
+    const json = await (await fetch(`https://61fbded03f1e34001792c680.mockapi.io/worldcupList`)).json();
+    console.log(json);
+    divisionWorldcupList(json);
+  };
+  const getSelectList = async () => {
+    const json = await (await fetch(`https://61fbded03f1e34001792c680.mockapi.io/selectList`)).json();
+    console.log(json);
+    divisionSelectList(json);
+  };
+  useEffect(() => {
+    getWorldcupList();
+    getSelectList();
+  }, []);
   return (
     <>
-      <Slider datas={datas} />
-      <Slider datas={datas} />
+      <div>이상형월드컵</div>
+      {worldcupLists.map((worldcuplist, i) => (
+        <Slider key={i} datas={worldcuplist} />
+      ))}
+      <div>대신정해주기</div>
+      {selectLists.map((selectlist, i) => (
+        <Slider key={i} datas={selectlist} />
+      ))}
     </>
   );
 };
