bdf085badd1c5e1bed2d8af06a6b823a03545a07
yunjin-kim <79692272+yunjin-kim@users.noreply.github.com>
2022-02-08 22:07:21 +0900

    Fix: mock 데이터 수정

--- app/src/mocks/handlers.js
+++ app/src/mocks/handlers.js
@@ -5,18 +5,21 @@ const handlers = [
     return res(
       ctx.status(200),
       ctx.delay(2000),
-      ctx.json([
-        {
-          name: "하트",
-          picture: "https://s3.marpple.co/f1/2019/1/1235206_1548918825999_78819.png",
-          tagList: "",
-        },
-        {
-          name: "도넛",
-          picture: "https://s3.marpple.co/f1/2019/1/1235206_1548918758054_55883.png",
-          tagList: "",
-        },
-      ]),
+      ctx.json({
+        title: "가지고 싶은 이모지 정해주기",
+        select: [
+          {
+            name: "하트",
+            picture: "https://s3.marpple.co/f1/2019/1/1235206_1548918825999_78819.png",
+            tagList: "",
+          },
+          {
+            name: "도넛",
+            picture: "https://s3.marpple.co/f1/2019/1/1235206_1548918758054_55883.png",
+            tagList: "",
+          },
+        ],
+      }),
     );
   }),
 ];
