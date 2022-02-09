fb648f97db445cbfa93dccbcf8185fadd1fcae3b
yunjin-kim <79692272+yunjin-kim@users.noreply.github.com>
2022-02-09 09:21:42 +0900

    Feat: 대신 정해주기 결과 msw 데이터 추가

--- app/src/mocks/handlers.js
+++ app/src/mocks/handlers.js
@@ -1,7 +1,7 @@
 import { rest } from "msw";
 
 const handlers = [
-  rest.get("/select/play", (req, res, ctx) => {
+  rest.get("/select/play/emoji", (req, res, ctx) => {
     return res(
       ctx.status(200),
       ctx.delay(2000),
@@ -17,6 +17,24 @@ const handlers = [
             name: "도넛",
             picture: "https://s3.marpple.co/f1/2019/1/1235206_1548918758054_55883.png",
             tagList: "",
+          },
+        ],
+      }),
+    );
+  }),
+  rest.get("/select/result/emoji", (req, res, ctx) => {
+    return res(
+      ctx.status(200),
+      ctx.delay(2000),
+      ctx.json({
+        select: [
+          {
+            name: "하트",
+            percent: "46%",
+          },
+          {
+            name: "도넛",
+            percent: "54%",
           },
         ],
       }),
