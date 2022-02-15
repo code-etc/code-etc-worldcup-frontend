d29523c3fec7990043355ce3ebc4f04d5de5b4b6
yunjin-kim <79692272+yunjin-kim@users.noreply.github.com>
2022-02-15 15:41:24 +0900

    Feat: 대신 정해주기 결과 렌더링 기능 추가

--- app/src/routes/PlaySelect.jsx
+++ app/src/routes/PlaySelect.jsx
@@ -1,24 +1,49 @@
 import React, { useEffect, useState } from "react";
 import PlaySelectData from "./PlaySelectData";
+import PlaySelectResult from "./PlaySelectResult";
 
 const PlaySelect = () => {
   const [selectTitle, setSelectTitle] = useState("");
-  const [selects, setSelects] = useState([]);
+  const [selectDatas, setSelectDatas] = useState([]);
+  const [clickSelect, setClickSelect] = useState(false);
+  const [selectResults, setSelectResults] = useState([]);
 
   useEffect(() => {
     getSelectDatas();
   }, []);
 
+  useEffect(() => {
+    if (clickSelect) {
+      getSelectResult();
+    }
+  }, [clickSelect]);
+
   const getSelectDatas = async () => {
-    const response = await fetch("/select/play");
+    const response = await fetch("/select/play/emoji");
     const data = await response.json();
-    setSelects([...data.select]);
+    setSelectDatas([...data.select]);
     setSelectTitle(data.title);
+    console.log(selectResults);
+  };
+
+  const getSelectResult = async () => {
+    const response = await fetch("/select/result/emoji");
+    const data = await response.json();
+    setSelectResults([...data.select]);
   };
 
   return (
     <>
-      <div>{selectTitle && <PlaySelectData selects={selects} selectTitle={selectTitle} />}</div>
+      <div>
+        <div className="overflow-hidden">
+          {selectTitle && (
+            <PlaySelectData selectDatas={selectDatas} selectTitle={selectTitle} setClickSelect={setClickSelect} />
+          )}
+        </div>
+        <div className="relative xl:top-[-100px] w-[100%] top-[-200px]">
+          {selectResults.length > 0 && <PlaySelectResult selectResults={selectResults} />}
+        </div>
+      </div>
     </>
   );
 };
