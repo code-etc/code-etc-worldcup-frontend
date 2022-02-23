c26a790b3a835702b7f275177bf086d07195a127
yunjin-kim <79692272+yunjin-kim@users.noreply.github.com>
2022-02-23 09:42:41 +0900

    Design: 폰트 색상, 후보자 위치 변경

--- app/src/routes/PlayWorldcuppage/PlayWorldcupData.jsx
+++ app/src/routes/PlayWorldcuppage/PlayWorldcupData.jsx
@@ -3,11 +3,14 @@ import React from "react";
 const PlayWorldcupData = ({ worldcupData, worldcupdataIndex, handleWorldcup }) => {
   return (
     <>
-      <div className={"w-[50%] h-[700px] relative cursor-pointer hover:scale-105"} onClick={handleWorldcup}>
-        <h3 className={"absolute text-[40px] " + (worldcupdataIndex === 0 ? "right-40" : "left-40")}>
+      <div
+        className={"w-[50%] h-[700px] relative cursor-pointer hover:scale-105 flex items-center"}
+        onClick={handleWorldcup}
+      >
+        <h3 className={"absolute text-[40px] text-sky-400  " + (worldcupdataIndex === 0 ? "right-40" : "left-40")}>
           {worldcupData.name}
         </h3>
-        <img src={worldcupData.picture} className="max-h-[100%] max-w-[100%] m-auto" alt={worldcupData.name} />
+        <img src={worldcupData.picture} className="max-h-[100%] max-w-[100%]" alt={worldcupData.name} />
       </div>
     </>
   );
