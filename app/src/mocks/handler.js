89b29592d6d786eae0b3986c1a4c7d35a69af461
yunjin-kim <79692272+yunjin-kim@users.noreply.github.com>
2022-02-23 18:51:52 +0900

    Feat: 월드컵 랭킹 더미 데이터

--- app/src/mocks/handler.js
+++ app/src/mocks/handler.js
@@ -72,7 +72,41 @@ const handlers = [
             tagList: "",
           },
         ],
-        "next-match": "/worldcup/result/wanttoeatmeat",
+      }),
+    );
+  }),
+  rest.get("/worldcup/rank/wanttoeatmeat", (req, res, ctx) => {
+    return res(
+      ctx.status(200),
+      ctx.delay(1000),
+      ctx.json({
+        title: "먹고 싶은 고기 월드컵",
+        rankList: [
+          {
+            name: "닭고기",
+            picture:
+              "https://images.unsplash.com/photo-1602534956586-2f4997a5a8ef?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8",
+            winRate: "40",
+          },
+          {
+            name: "돼지고기",
+            picture:
+              "https://images.unsplash.com/photo-1633358800265-3a5a8f7a7cf9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8",
+            winRate: "30",
+          },
+          {
+            name: "양고기",
+            picture:
+              "https://images.unsplash.com/photo-1432139509613-5c4255815697?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8",
+            winRate: "12",
+          },
+          {
+            name: "소고기",
+            picture:
+              "https://images.unsplash.com/photo-1619719015339-133a130520f6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8",
+            winRate: "4",
+          },
+        ],
       }),
     );
   }),
