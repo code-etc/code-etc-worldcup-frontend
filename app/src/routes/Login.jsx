78f44db16cf800e38753342608ce776281ed8fcc
daehwan2 <dhdh0482@naver.com>
2022-02-27 21:27:39 +0900

    Fix: 로그인 페이지 삭제 ( 앞에 커밋실수)

--- app/src/routes/Login.jsx
+++ app/src/routes/Login.jsx
@@ -2,13 +2,7 @@ import React, { useEffect } from "react";
 
 const Login = () => {
   useEffect(() => {}, []);
-  return (
-    <div>
-      <a href="http://localhost:8080/oauth2/authorize/google?redirect_uri=http://localhost:3000/oauth2/redirect/">
-        로그인
-      </a>
-    </div>
-  );
+  return <div></div>;
 };
 
 export default Login;
