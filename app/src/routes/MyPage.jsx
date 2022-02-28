ec01fedba96b2da03e5144efae36615a7329ca02
daehwan2 <dhdh0482@naver.com>
2022-02-28 19:33:06 +0900

    Ref: 별명, 거주지, 나이 jsx 리팩토링 및 state추가

--- app/src/routes/MyPage.jsx
+++ app/src/routes/MyPage.jsx
@@ -5,6 +5,9 @@ const MyPage = () => {
   const [mySelectList, setMySelectList] = useState([]);
   const [worldcupDisplay, setWorldcupDisplay] = useState(false);
   const [selectDisplay, setSelectDisplay] = useState(false);
+  const [nickname, setNickname] = useState("");
+  const [place, setPlace] = useState("");
+  const [age, setAge] = useState(0);
   const refWorldcupBtn = useRef();
   const refSelectBtn = useRef();
   const refWorldcupList = useRef();
@@ -25,6 +28,24 @@ const MyPage = () => {
   const displaySelectHandler = () => {
     setSelectDisplay((prev) => !prev);
   };
+  const getUser = () => {
+    // 유저 정보 호출
+    setNickname("김대환");
+    setPlace("부산");
+    setAge(25);
+  };
+  const nicknameHandler = (e) => {
+    setNickname(e.target.value);
+  };
+  const placeHandler = (e) => {
+    setPlace(e.target.value);
+  };
+  const ageHandler = (e) => {
+    setAge(e.target.value);
+  };
+  const onClickInput = (e) => {
+    e.preventDefault();
+  };
   useEffect(() => {
     if (worldcupDisplay) {
       refWorldcupBtn.current.classList.remove("rotate-180");
@@ -42,6 +63,7 @@ const MyPage = () => {
   useEffect(() => {
     getMyWorldcup();
     // getMySelect();
+    getUser();
   }, []);
   return (
     <div className="mainFont">
@@ -54,33 +76,68 @@ const MyPage = () => {
         </header>
 
         <main>
-          <div className="flex text-[18px] mb-[24px] md:text-[28px]">
-            <div className="flex flex-col mr-[10px]">
-              <label htmlFor="nickname" className="mb-[5px]">
-                별명
-              </label>
-              <label htmlFor="place" className="mb-[5px]">
-                거주지
-              </label>
-              <label htmlFor="age">나이</label>
-            </div>
-
-            <div className="flex flex-col">
+          <div className="flex flex-col text-[20px] mb-[18px] md:text-[30px] md:mb-[24px]">
+            <div className="flex">
+              <div className="flex flex-col justify-center mr-[10px] w-[52px] md:mr-[18px] md:w-[80px]">
+                <label
+                  htmlFor="nickname"
+                  className="cursor-pointer focus:text-[#0554f2] active:text-[#0554f2] hover:text-[#0554f2] ease-in-out	duration-200"
+                >
+                  별명
+                </label>
+                <div className="text-[8px] mt-[-4px] md:text-[12px] md:mt-[-6px]">클릭해서 수정</div>
+              </div>
               <input
                 id="nickname"
-                className="mb-[5px] border-b-[2px] border-black"
+                className="mb-[5px] border-b-[2px] border-black pointer-events-none"
                 type="text"
                 placeholder="별명를 입력해주세요"
+                value={nickname}
+                onChange={nicknameHandler}
               />
+            </div>
+
+            <div className="flex">
+              <div className="flex flex-col justify-center mr-[10px] w-[52px] md:mr-[18px] md:w-[80px]">
+                <label
+                  htmlFor="place"
+                  className="cursor-pointer focus:text-[#0554f2] active:text-[#0554f2] hover:text-[#0554f2] ease-in-out	duration-200"
+                >
+                  거주지
+                </label>
+                <div className="text-[8px] mt-[-4px] md:text-[12px] md:mt-[-6px]">클릭해서 수정</div>
+              </div>
               <input
                 id="place"
-                className="mb-[5px] border-b-[2px] border-black"
+                className="mb-[5px] border-b-[2px] border-black pointer-events-none"
                 type="text"
                 placeholder="거주지를 입력해주세요"
+                value={place}
+                onChange={placeHandler}
               />
-              <input id="age" className="border-b-[2px] border-black" type="text" placeholder="나이를 입력해주세요" />
+            </div>
+
+            <div className="flex">
+              <div className="flex flex-col justify-center mr-[10px] w-[52px] md:mr-[18px] md:w-[80px]">
+                <label
+                  htmlFor="age"
+                  className="cursor-pointer focus:text-[#0554f2] active:text-[#0554f2] hover:text-[#0554f2] ease-in-out	duration-200"
+                >
+                  나이
+                </label>
+                <div className="text-[8px] mt-[-4px] md:text-[12px] md:mt-[-6px]">클릭해서 수정</div>
+              </div>
+              <input
+                id="age"
+                className="border-b-[2px] border-black pointer-events-none"
+                type="number"
+                placeholder="나이를 입력해주세요"
+                value={age}
+                onChange={ageHandler}
+              />
             </div>
           </div>
+
           <div className="md:flex md:justify-between">
             <div className="mb-[15px] basis-2/5">
               <div className="flex justify-between">
