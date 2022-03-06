b0e604027fc6b40fa88a96e5b897a006d07d379f
yunjin-kim <79692272+yunjin-kim@users.noreply.github.com>
2022-03-06 18:57:15 +0900

    Design: 부전승 텍스트 css  수정

--- app/src/routes/PlayWorldcuppage/PlayWorldcupDatas.jsx
+++ app/src/routes/PlayWorldcuppage/PlayWorldcupDatas.jsx
@@ -13,12 +13,14 @@ const PlayWorldcupDatas = ({ worldcupDatas, setClickWorldcup, worldcupCount, set
         {worldcupDatas.length === 1 ? (
           <>
             <div className="w-[200px] height-[100%] flex justify-center items-center" onClick={chooseWorldcup}>
-              <p className=" text-[40px] cursor-pointer text-sky-400">통과</p>
+              <p className="xl:text-[100px] sm:text-[80px] text-[60px] cursor-pointer">통과~</p>
             </div>
-            <h1 className="xl:text-[150px] sm:text-[100px] absolute w-fix z-10 text-[50px]">부전승 발생</h1>
+            <h1 className="xl:text-[150px] sm:text-[100px] text-[80px] absolute w-fix z-10 text-red-600">
+              부전승 발생!
+            </h1>
             <PlayWorldcupData worldcupData={worldcupDatas[0]} />
             <div className="w-[200px] height-[100%] flex justify-center items-center" onClick={chooseWorldcup}>
-              <p className=" text-[40px] cursor-pointer text-red-600">탈락</p>
+              <p className="xl:text-[100px] sm:text-[80px] text-[60px] cursor-pointer">탈락..</p>
             </div>
           </>
         ) : (
