1ad9677e6f0b97b5dd7fce74309f8fc6ffed659c
yunjin-kim <79692272+yunjin-kim@users.noreply.github.com>
2022-03-06 18:49:02 +0900

    fix: 월드컵 msw 데이터 수정

--- app/src/mocks/handler.js
+++ app/src/mocks/handler.js
@@ -1,127 +1,141 @@
 import { rest } from "msw";
 
 const handlers = [
-  rest.get("/worldcup/play/wanttoeatmeat/1-3", (req, res, ctx) => {
+  rest.get("/worldcup/play/wanttogocity/1-4", (req, res, ctx) => {
     return res(
       ctx.status(200),
-      ctx.delay(1000),
       ctx.json({
-        worldcupCount: [1, 3],
-        title: "먹고 싶은 고기 월드컵",
+        worldcupCount: [1, 4],
+        title: "가고 싶은 도시 월드컵",
         select: [
           {
-            name: "돼지고기",
+            name: "서울",
             picture:
-              "https://images.unsplash.com/photo-1633358800265-3a5a8f7a7cf9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8",
+              "https://images.unsplash.com/photo-1532085755448-a67fd3b518b2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8",
             tagList: "",
           },
           {
-            name: "소고기",
+            name: "도쿄",
             picture:
-              "https://images.unsplash.com/photo-1619719015339-133a130520f6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8",
+              "https://images.unsplash.com/photo-1503899036084-c55cdd92da26?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8",
             tagList: "",
           },
         ],
-        "next-match": "/worldcup/play/wanttoeatmeat/2-3",
+        "next-match": "/worldcup/play/wanttogocity/2-4",
       }),
     );
   }),
-  rest.get("/worldcup/play/wanttoeatmeat/2-3", (req, res, ctx) => {
+  rest.get("/worldcup/play/wanttogocity/2-4", (req, res, ctx) => {
     return res(
       ctx.status(200),
-      ctx.delay(1000),
       ctx.json({
-        worldcupCount: [2, 3],
-        title: "먹고 싶은 고기 월드컵",
+        worldcupCount: [2, 4],
+        title: "가고 싶은 도시 월드컵",
         select: [
           {
-            name: "닭고기",
+            name: "뉴욕",
             picture:
-              "https://images.unsplash.com/photo-1602534956586-2f4997a5a8ef?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8",
+              "https://images.unsplash.com/photo-1510673354311-c81bb8506fc2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8",
             tagList: "",
           },
           {
-            name: "양고기",
+            name: "런던",
             picture:
-              "https://images.unsplash.com/photo-1432139509613-5c4255815697?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8",
+              "https://images.unsplash.com/photo-1513635269975-59663e0ac1ad?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8",
             tagList: "",
           },
         ],
-        "next-match": "/worldcup/play/wanttoeatmeat/3-3",
+        "next-match": "/worldcup/play/wanttogocity/3-4",
       }),
     );
   }),
-  rest.get("/worldcup/play/wanttoeatmeat/3-3", (req, res, ctx) => {
+  rest.get("/worldcup/play/wanttogocity/3-4", (req, res, ctx) => {
     return res(
       ctx.status(200),
-      ctx.delay(1000),
       ctx.json({
-        worldcupCount: [3, 3],
-        title: "먹고 싶은 고기 월드컵",
+        worldcupCount: [3, 4],
+        title: "가고 싶은 도시 월드컵",
         select: [
           {
-            name: "닭고기",
+            name: "홍콩",
             picture:
-              "https://images.unsplash.com/photo-1602534956586-2f4997a5a8ef?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8",
+              "https://images.unsplash.com/photo-1558281686-c8514528eab0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8",
+            tagList: "",
+          },
+        ],
+        "next-match": "/worldcup/play/wanttogocity/4-4",
+      }),
+    );
+  }),
+  rest.get("/worldcup/play/wanttogocity/4-4", (req, res, ctx) => {
+    return res(
+      ctx.status(200),
+      ctx.json({
+        worldcupCount: [4, 4],
+        title: "가고 싶은 도시 월드컵",
+        select: [
+          {
+            name: "서울",
+            picture:
+              "https://images.unsplash.com/photo-1532085755448-a67fd3b518b2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8",
             tagList: "",
           },
           {
-            name: "돼지고기",
+            name: "뉴욕",
             picture:
-              "https://images.unsplash.com/photo-1633358800265-3a5a8f7a7cf9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8",
+              "https://images.unsplash.com/photo-1510673354311-c81bb8506fc2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8",
             tagList: "",
           },
         ],
       }),
     );
   }),
-  rest.get("/worldcup/rank/wanttoeatmeat", (req, res, ctx) => {
+  rest.get("/worldcup/rank/wanttogocity", (req, res, ctx) => {
     return res(
       ctx.status(200),
-      ctx.delay(1000),
       ctx.json({
-        title: "먹고 싶은 고기 월드컵",
+        title: "가고 싶은 도시 월드컵",
         rankList: [
           {
-            name: "닭고기",
+            name: "뉴욕",
             picture:
-              "https://images.unsplash.com/photo-1602534956586-2f4997a5a8ef?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8",
+              "https://images.unsplash.com/photo-1510673354311-c81bb8506fc2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8",
             winRate: "40",
           },
           {
-            name: "돼지고기",
+            name: "서울",
             picture:
-              "https://images.unsplash.com/photo-1633358800265-3a5a8f7a7cf9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8",
+              "https://images.unsplash.com/photo-1532085755448-a67fd3b518b2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8",
             winRate: "30",
           },
           {
-            name: "양고기",
+            name: "홍콩",
             picture:
-              "https://images.unsplash.com/photo-1432139509613-5c4255815697?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8",
+              "https://images.unsplash.com/photo-1558281686-c8514528eab0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8",
             winRate: "12",
           },
           {
-            name: "소고기1",
+            name: "런던",
             picture:
-              "https://images.unsplash.com/photo-1619719015339-133a130520f6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8",
+              "https://images.unsplash.com/photo-1513635269975-59663e0ac1ad?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8",
             winRate: "4",
           },
           {
-            name: "소고기2",
+            name: "도쿄",
             picture:
-              "https://images.unsplash.com/photo-1619719015339-133a130520f6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8",
+              "https://images.unsplash.com/photo-1503899036084-c55cdd92da26?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8",
             winRate: "2",
           },
           {
-            name: "소고기3",
+            name: "LA",
             picture:
-              "https://images.unsplash.com/photo-1619719015339-133a130520f6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8",
+              "https://images.unsplash.com/photo-1542737579-ba0a385f3b84?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8",
             winRate: "0.2",
           },
           {
-            name: "소고기4",
+            name: "시드니",
             picture:
-              "https://images.unsplash.com/photo-1619719015339-133a130520f6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8",
+              "https://images.unsplash.com/photo-1506973035872-a4ec16b8e8d9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8",
             winRate: "0.1",
           },
         ],
