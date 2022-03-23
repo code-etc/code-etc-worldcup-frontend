f38a7999ed91917557a1f261a14a2a8b50166827
daehwan2 <dhdh0482@naver.com>
2022-03-24 00:20:29 +0900

    Feat: BackgroundImage 로드이후에 렌더링하기 #15

--- app/src/components/Slider.jsx
+++ app/src/components/Slider.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useRef, useEffect } from "react";
+import { BackgroundImage } from "react-image-and-background-image-fade";
 import styles from "./Slider.module.css";
 const Slide = ({ datas }) => {
   const ulRef = useRef(null);
@@ -6,6 +7,8 @@ const Slide = ({ datas }) => {
   const timeoutRef = useRef(null);
   const [itemIndex, setItemIndex] = useState(0);
   const first = useRef(true);
+  const [isLoading, setIsLoading] = useState(true);
+  const [rendering, setRendering] = useState(false);
   const isTouching = useRef(false);
   const isButtonClick = useRef(false);
   const touchStartPoint = useRef({
@@ -17,6 +20,7 @@ const Slide = ({ datas }) => {
     y: -1,
   });
 
+  let loadCount = 0;
   const ITEM_NUMBER = datas.length;
   const [VISIBLE_NUMBER, setVisibleNumber] = useState(1);
   const handleResize = () => {
@@ -57,7 +61,6 @@ const Slide = ({ datas }) => {
       clearTimeout(timeoutRef.current);
       timeoutRef.current = setInterval(() => {
         setItemIndex((prev) => prev + 1);
-        console.log("hi");
       }, 3000);
       isButtonClick.current = false;
     }
@@ -69,6 +72,7 @@ const Slide = ({ datas }) => {
       ulRef.current.style.transform = `translateX(-${(100 / ITEM_NUMBER) * itemIndex}%)`;
     }
   }, [itemIndex]);
+
   return (
     <>
       <div className={styles.container}>
@@ -83,10 +87,8 @@ const Slide = ({ datas }) => {
             touchEndPoint.current.x = e.changedTouches[0].clientX;
             touchEndPoint.current.y = e.changedTouches[0].clientY;
             if (touchStartPoint.current.x - touchEndPoint.current.x > 10) {
-              console.log("hi");
               setItemIndex((prev) => prev + 1);
             } else if (touchEndPoint.current.x - touchStartPoint.current.x > 10) {
-              console.log("hi2");
               setItemIndex((prev) => prev - 1);
             }
             isTouching.current = false;
@@ -94,23 +96,38 @@ const Slide = ({ datas }) => {
           ref={ulRef}
         >
           {datas.map((data) => (
-            <li
-              style={{ backgroundImage: `url(${data.thumbnail})` }}
+            <BackgroundImage
+              src={data.thumbnail}
               ref={liRef}
               key={data.id}
               className={styles.slide_item}
+              renderLoader={({ hasLoaded, hasFailed }) => {
+                if (hasLoaded) {
+                  loadCount++;
+                  if (loadCount === datas.length) {
+                    setIsLoading(false);
+                    console.log(datas.length, loadCount);
+                  }
+                }
+              }}
             >
-              <strong className={styles.title}>{data.title}</strong>
-              <div className={styles.button_container}>
-                <a className={styles.start_button} href="/">
-                  시작하기
-                </a>
-                <a className={styles.rank_button} href="/">
-                  랭킹보기
-                </a>
-              </div>
-              <div className={styles.author}>Made by:{data.author}</div>
-            </li>
+              {isLoading ? (
+                <div className="text-white absolute">로딩중</div>
+              ) : (
+                <>
+                  <strong className={styles.title}>{data.title}</strong>
+                  <div className={styles.button_container}>
+                    <a className={styles.start_button} href="/">
+                      시작하기
+                    </a>
+                    <a className={styles.rank_button} href="/">
+                      랭킹보기
+                    </a>
+                  </div>
+                  <div className={styles.author}>Made by:{data.author}</div>
+                </>
+              )}
+            </BackgroundImage>
           ))}
         </ul>
         <button
